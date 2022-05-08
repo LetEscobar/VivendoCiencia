3e7fb0d8e27ff62b0774aed91521d6a31b8cc66e
Letícia Escobar <leticia_escobar@ufms.br>
2022-05-08 01:25:24 -0300

    Projeto final

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
-// PAREI NA AULA 5 - TEMPO: 1:30
-
 window.addEventListener('scroll', onScroll)
 
+onScroll()
 function onScroll() {
     showNavOnScroll()
-    showBackToButtonOnScroll()
+    showBackToTopButtonOnScroll()
+
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+    // criando a linha alvo
+    const targetLine = scrollY + innerHeight / 2
+
+    // verificar se a sessão passou da linha
+    // capturando os dados que são necessários
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+    // informações dos dados e da lógica
+
+    // verificar se a base está abaix a liha alvo
+    // quais dados vou precisar?
+    const sectionEndsAt = sectionTop + sectionHeight
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+    // limites da sessão
+    const sectionBoundaries =
+        sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll() {
@@ -15,7 +50,7 @@ function showNavOnScroll() {
     }
 }
 
-function showBackToButtonOnScroll() {
+function showBackToTopButtonOnScroll() {
     if (scrollY > 550) {
         backToTopButton.classList.add('show')
     } else {
