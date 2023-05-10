dd986aa568e1bc022835a4673dc91932a13905e5
LetEscobar <leticiaescobaraujo@gmail.com>
2023-05-10 17:03:12 -0300

    Envio de email funcionando, sem enviar atividades

--- enviar_formulario.php
+++ enviar_formulario.php
@@ -1,22 +1,48 @@
 <?php
+// Inclua os arquivos necessários do PHPMailer
+require 'PHPMailer-master\src\PHPMailer.php';
+require 'PHPMailer-master\src\SMTP.php';
+require 'PHPMailer-master\src\Exception.php';
+
+// Verifique se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recupere os dados do formulário
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $instagram = $_POST["instagram"];
-    $comprovante = $_FILES["comprovante"]["name"];
+    $comprovante_nome = $_FILES["comprovante"]["name"];
+    $comprovante_temp = $_FILES["comprovante"]["tmp_name"];
 
-    $to = "leticiaescobaraujo@gmail.com"; // Insira o endereço de e-mail para onde deseja enviar os dados
-    $subject = "Novo pedido de compra";
-    $message = "Nome: " . $nome . "\n";
-    $message .= "E-mail: " . $email . "\n";
-    $message .= "Perfil do Instagram ou número do WhatsApp: " . $instagram . "\n";
-    $message .= "Comprovante de Pagamento (PIX): " . $comprovante . "\n";
+    // Crie uma instância do PHPMailer
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
 
-    // Envie o e-mail
-    mail($to, $subject, $message);
+    // Configurações do servidor SMTP
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'contato.vivendociencia@gmail.com';
+    $mail->Password = 'hgtkoabuvxvjocvk';
+    $mail->Port = 587;
 
-    // Redirecione para uma página de confirmação
-    header("Location: confirmacao.html");
-    exit;
+    // Configurações do email
+    $mail->setFrom('contato.vivendociencia@gmail.com', 'VivendoCiencia');
+    $mail->addAddress('contato.vivendociencia@gmail.com', 'VivendoCiencia');
+    $mail->Subject = 'Novo pedido de compra';
+    $mail->Body = "Nome: " . $nome . "\n";
+    $mail->Body .= "E-mail: " . $email . "\n";
+    $mail->Body .= "Perfil do Instagram ou número do WhatsApp: " . $instagram . "\n";
+    $mail->Body .= "Comprovante de Pagamento em anexo. " . $comprovante . "\n";
+
+    // Anexar o comprovante como anexo
+    $mail->addAttachment($comprovante_temp, $comprovante_nome);
+
+    // Verifique se o email foi enviado com sucesso
+    if ($mail->send()) {
+        // Redirecione para uma página de confirmação
+        header("Location: confirmacao.html");
+        exit;
+    } else {
+        // Exiba uma mensagem de erro, se houver algum problema no envio do email
+        echo "Erro ao enviar o email: " . $mail->ErrorInfo;
+    }
 }
-?>
